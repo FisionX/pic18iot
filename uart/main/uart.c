7eb33629e54fdd0d83f28f4a6094651afdf7fcc6
fernando <fherpato8@gmail.com>
2025-06-10 11:27:05 -0600

    Modified uart program

--- uart/main/uart.c
+++ uart/main/uart.c
@@ -1,39 +1,39 @@
-#include <stdint.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/queue.h"
-#include "esp_log.h"
+#include "driver/uart.h"
 #include "driver/gpio.h"
 #include "hal/uart_types.h"
 #include "portmacro.h"
-#include "driver/uart.h"
 
-QueueHandle_t servoQueue;
 static void IRAM_ATTR uart_task(void* pvParameters)
 {
     uint8_t data[6];
-    data[0] = 20;
-    data[1] = 40;
-    data[2] = 50;
-    data[3] = 60;
-    data[4] = 50;
-    data[5] = 00;
-    uart_write_bytes(UART_NUM_2, data, 6);
-    vTaskDelay(1000/portTICK_PERIOD_MS);
-    data[0] = 50;
-    data[1] = 20;
-    data[2] = 70;
-    data[3] = 20;
-    data[4] = 40;
-    data[5] = 40;
-    uart_write_bytes(UART_NUM_2, data, 6);
-    vTaskDelay(1000/portTICK_PERIOD_MS);
+    for (;;) {
+        data[0] = 20;
+        data[1] = 40;
+        data[2] = 50;
+        data[3] = 60;
+        data[4] = 50;
+        data[5] = 00;
+        printf("Pos 1\n");
+        uart_write_bytes(UART_NUM_0, (const char*)data, 6);
+        //vTaskDelay(1000/portTICK_PERIOD_MS);
+        data[0] = 50;
+        data[1] = 20;
+        data[2] = 70;
+        data[3] = 20;
+        data[4] = 40;
+        data[5] = 40;
+        printf("Pos 2\n");
+        uart_write_bytes(UART_NUM_0, (const char*)data, 6);
+        //vTaskDelay(1000/portTICK_PERIOD_MS);
+    }
 
 }
 static void uart_init() {
-    uart_port_t uart_num = UART_NUM_2;
+    uart_port_t uart_num = UART_NUM_0;
     uart_config_t uart_config = {
         .baud_rate = 115200,
         .data_bits = UART_DATA_8_BITS,
@@ -45,17 +45,21 @@ static void uart_init() {
     ESP_ERROR_CHECK(uart_param_config(uart_num, &uart_config));
     
     // Set UART pins(TX: IO4, RX: IO5, RTS: IO18, CTS: IO19)
-    ESP_ERROR_CHECK(uart_set_pin(UART_NUM_2, 4, 5, 18, 19));
+    ESP_ERROR_CHECK(uart_set_pin(uart_num, 1, 3, -1, -1));
 
     // Setup UART buffered IO with event queue
     const int uart_buffer_size = (1024 * 2);
     QueueHandle_t uart_queue;
     // Install UART driver using an event queue here
-    ESP_ERROR_CHECK(uart_driver_install(UART_NUM_2, uart_buffer_size, \
+    ESP_ERROR_CHECK(uart_driver_install(uart_num, uart_buffer_size, \
                                             uart_buffer_size, 10, &uart_queue, 0));
-    xTaskCreate(uart_init, "uart_motor", 2048, NULL, 5, NULL);
+    xTaskCreate(uart_task, "uart_motor", 1024, NULL, 5, NULL);
 }
 void app_main(void)
 {
-    uart_init();
+    //uart_init();
+    for(;;)
+    {
+        vTaskDelay(100/portTICK_PERIOD_MS);
+    }
 }
